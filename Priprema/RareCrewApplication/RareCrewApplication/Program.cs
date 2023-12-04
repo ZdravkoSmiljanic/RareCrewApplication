58137defcd79533de0f9929cc365f396f29cacd3
ZdravkoSmiljanic <smiljanic070201@gmail.com>
2023-12-04 19:42:51 +0100

    Changed path

--- Priprema/RareCrewApplication/RareCrewApplication/Program.cs
+++ Priprema/RareCrewApplication/RareCrewApplication/Program.cs
@@ -18,21 +18,22 @@ namespace RareCrewApplication
         {
 
             //pre pokretanja programa mozete obrisati html stranicu i sliku iz foldera
-            //pagePart i pngPath zameniti na racunaru gde se pokrece
+            
             GenerateHtmlPage page = new GenerateHtmlPage();
             GeneratePiePng pie = new GeneratePiePng();
             EmpolyeeController controller = new EmpolyeeController();
-           
-          
+
+            //dovatanje foldera u kom se nalazi aplikacija
+            var currentDirectory = System.IO.Directory.GetCurrentDirectory();
             //Dohvatanje podataka sa API-ja
             List<TotalWorkTime> employees1 = await controller.GetEmployeeData();
             //kreiranje stranice sa tabelom
             string html = page.GenerateHtmlTable(employees1);
-            string pagePath = "C:\\Users\\User\\Desktop\\Priprema\\RareCrewApplication\\employes.html";
+            string pagePath = currentDirectory + "\\employes.html";
             File.WriteAllText(pagePath, html);
             Console.WriteLine("Html page successfully created");
             //Kreiranje slike sa pie-chartom prikaza podataka o vremenu radnika
-            string pngPath = "C:\\Users\\User\\Desktop\\Priprema\\RareCrewApplication\\pie.png";
+            string pngPath = currentDirectory + "\\pie.png";
             pie.GeneratePieChart(employees1, pngPath);
             Console.WriteLine("Pie image successfully created");
 
